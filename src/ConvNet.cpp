6b2155fcc35c86c21983126feaef62914cff9a5f
kudas829 <136510339+kudas829@users.noreply.github.com>
2023-07-23 16:07:47 -0500

    window
    window and glfw setup

--- src/ConvNet.cpp
+++ src/ConvNet.cpp
@@ -1,7 +1,27 @@
 ﻿#include <iostream>
+#include <GLFW/glfw3.h>
 
 int main()
 {
+	GLFWwindow* window;
+
+	glfwInit();
+
+	window = glfwCreateWindow(1080, 720, "Hello GLFW", NULL, NULL);
+
+	glfwMakeContextCurrent(window);
+
+	while(!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
 	std::cout << "Hello CMake. morbidly a beast" << std::endl;
 
 	std::cin.get();
